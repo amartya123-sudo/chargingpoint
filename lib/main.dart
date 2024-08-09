884385954bd23ae1d9adf320defffc5b911586a7
Amartya Saran <amartyasaran@gmail.com>
2024-08-09 11:16:01 +0530

    .env removed

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:charginpoint/overview.dart';
 
 void main() async {
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: '../.env');
 
   runApp(const MyApp());
 }
